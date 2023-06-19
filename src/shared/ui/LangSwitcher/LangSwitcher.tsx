3eb29d6be77792b75340ab8afbbbb40b698f8cbb
Sergei Shatohin <trapjkeeee@gmail.com>
2023-06-19 15:51:38 +0300

    add i18n

---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,28 @@
+import { classNames } from "helpers/classNames/classNames";
+import { useTranslation } from "react-i18next";
+import cls from "./LangSwitcher.module.scss";
+import Button, { ThemeButton } from "../Button/Button";
+
+interface LangSwitcherProps {
+  className?: string;
+}
+
+const LangSwitcher = ({ className }: LangSwitcherProps) => {
+  const { t, i18n } = useTranslation();
+
+  const toggle = async () => {
+    i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
+  };
+
+  return (
+    <Button
+      className={classNames(cls.LangSwitcher, {}, [className])}
+      theme={ThemeButton.CLEAR}
+      onClick={toggle}
+    >
+      {t("Язык")}
+    </Button>
+  );
+};
+
+export default LangSwitcher;
