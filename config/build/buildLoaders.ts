6b1e0bf07b43e79302eff997c6165bda285faa2f
Sergei Shatohin <trapjkeeee@gmail.com>
2023-06-19 17:29:38 +0300

    add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -42,5 +42,22 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
+  const babaelLoader = {
+    test: /\.(js|jsx|tsx)/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            { locales: ["ru", "en"], keyAsDefaultValue: true },
+          ],
+        ],
+      },
+    },
+  };
+
+  return [fileLoader, svgLoader, babaelLoader, typescriptLoader, cssLoader];
 }
