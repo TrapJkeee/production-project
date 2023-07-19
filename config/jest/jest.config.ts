ca4ab78bc663880dbc52109bd82ca854bee1009b
Sergei Shatohin <trapjkeeee@gmail.com>
2023-07-19 13:56:17 +0300

    fix svg

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -33,7 +33,7 @@ export default {
     setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
-        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+        '\\.svg': ['<rootDir>config/jest/jestEmptyComponent.tsx'],
     },
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
